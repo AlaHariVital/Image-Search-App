dd15a0eaadaba8ee1f30f1cdadaa32b9d70eeafa
AlaHariVital <100507619+AlaHariVital@users.noreply.github.com>
2023-09-14 05:47:18 +0530

    feat: error message added for the invalid search term

--- index.js
+++ index.js
@@ -12,33 +12,47 @@ let inputData = '';
 let page = 1;
 
 async function searchImages() {
-  inputData = searchInputElement.value;
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${apiKey}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  if (page === 1) {
-    searchResultsElement.innerHTML = '';
-  }
-  const results = data.results;
+  try {
+    inputData = searchInputElement.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${apiKey}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.results.length === 0) {
+      throw new error();
+    } else {
+      if (page === 1) {
+        searchResultsElement.innerHTML = '';
+      }
+      const results = data.results;
 
-  results.map((result) => {
-    const imageWrapper = document.createElement('div');
-    imageWrapper.classList.add('search-result');
-    const image = document.createElement('img');
-    image.src = result.urls.small;
-    image.alt = result.alt_description;
-    const imageLink = document.createElement('a');
-    imageLink.href = result.links.html;
-    imageLink.target = '_blank';
-    imageLink.textContent = result.alt_description;
+      results.map((result) => {
+        const imageWrapper = document.createElement('div');
+        imageWrapper.classList.add('search-result');
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement('a');
+        imageLink.href = result.links.html;
+        imageLink.target = '_blank';
+        imageLink.textContent = result.alt_description;
 
-    imageWrapper.appendChild(image);
-    imageWrapper.appendChild(imageLink);
-    searchResultsElement.appendChild(imageWrapper);
-  });
-  page++;
-  if (page > 1) {
-    showMoreButtonElement.style.display = 'block';
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResultsElement.appendChild(imageWrapper);
+      });
+      page++;
+      if (page > 1) {
+        showMoreButtonElement.style.display = 'block';
+      }
+    }
+  } catch (error) {
+    const errorElement = document.createElement('p');
+    errorElement.textContent =
+      'There are non images with the search term ! Please search another term';
+    errorElement.style.color = 'red';
+    errorElement.style.fontSize = '18px';
+    errorElement.style.alignItems = 'center';
+    searchResultsElement.appendChild(errorElement);
   }
 }
 
